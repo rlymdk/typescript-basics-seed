7348556dcda847bcf3d7c473da521e90d5db03f4
rlymdk <madikktl@gmail.com>
2018-07-06 00:44:23 +0600

    arrow functions

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Yo, TypeScript!!!!');
+const pizzas = [
+  { name: 'Pepperoni', toppings: ['pepperoni']}
+]
+
+const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase())
+
+console.log(mappedPizzas)
+
+const pizza = {
+  name: 'Blazing Inferno',
+  getName: () => pizza.name
+}
+
+console.log(pizza.getName())
