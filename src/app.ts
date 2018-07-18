339525e944381074d235fd217bbd3c0daab2db56
rlymdk <madikktl@gmail.com>
2018-07-18 13:14:55 +0600

    default params

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,7 @@
 let sumOrder: (price: number, quantity?: number) => number
 
-sumOrder = (x, y) => {
-  if (y) {
-    return x * y
-  }
+sumOrder = (x, y = 1) => x * y
 
-  return x
-}
-
-const sum = sumOrder(25)
-
-sumOrder(25, 2)
+const sum = sumOrder(25, 5)
 
 console.log(`Total sum: ${sum}`)
