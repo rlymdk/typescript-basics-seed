4a2d48d00d80bfea9488aa1abd8abda26db33bc1
rlymdk <madikktl@gmail.com>
2018-07-19 14:16:00 +0600

    type assertion

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
-type Size = 'small' | 'medium' | 'large'
-type Callback = (size: Size) => void
+type Pizza = { name: string, toppings: number }
 
-let pizzaSize: Size = 'small'
+const pizza: Pizza = { name: 'Blazing Inferno', toppings: 5 }
 
-const selectSize:Callback = (x) => {
-  pizzaSize = x
+const serialized = JSON.stringify(pizza)
+
+function getNameFromJson(obj: string) {
+  return (JSON.parse(obj) as Pizza).name
 }
 
-selectSize('small')
-
-console.log(pizzaSize)
+getNameFromJson(serialized)
