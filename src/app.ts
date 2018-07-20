a23c346bec5ec9d2418bc61e7b53b7223d4518ce
rlymdk <madikktl@gmail.com>
2018-07-20 10:55:54 +0600

    set get super

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,27 @@
-class Pizza {
+class Sizes {
+  constructor(public sizes: string[]) {}
+
+  set availableSizes(sizes: string[]) {
+    this.sizes = sizes
+  }
+
+  get availableSizes() {
+    return this.sizes
+  }
+}
+
+class Pizza extends Sizes {
   toppings: string[] = []
 
-  constructor(private name: string) {}
+  constructor(readonly name: string, public sizes: string[]) {
+    super(sizes)
+  }
 
   addTopping(topping: string) {
     this.toppings.push(topping)
   }
 }
 
-const pizza = new Pizza('Pepperoni')
-
+const pizza = new Pizza('Pepperoni', ['small', 'medium'])
+console.log(pizza.availableSizes)
 pizza.addTopping('pepperoni')
-
-console.log(pizza)
